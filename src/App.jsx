f68886c1113b922b5abba52e7331dfe9f5dda8b4
Harish <harish0916ojacs@gmail.com>
2026-03-27 01:53:20 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import princ from './assets/princ.jpeg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,9 +12,7 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+          <img src={princ} alt="" height={'100px'} />          
         </div>
         <div>
           <h1>NexvoraTech</h1>
